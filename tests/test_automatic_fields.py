41fcc0d0c8034941df343fd8f28b597969de5bb3
satyendra101 <satyendra.singh@qlik.com>
2026-04-21 10:48:28 +0000

    Update automatic tests

--- tests/test_automatic_fields.py
+++ tests/test_automatic_fields.py
@@ -19,11 +19,24 @@ class SAPSalesServiceCloudAutomaticFieldsTest(MinimumSelectionTest, SAPSalesServ
     # EG: For this stream: EmployeeUserSubscriptionAssignmentUserSubscriptionTypeCodeCollection, we are
     # getting multiple records with same value for primary key field "Code".
     # This is causing the test to fail as tap-tester expects unique values for primary key fields.
-    # Endponint to test: https://my369947.crm.ondemand.com/sap/c4c/odata/v1/c4codataapi/EmployeeUserSubscriptionAssignmentUserSubscriptionTypeCodeCollection?$format=json&$filter=Code eq '8003186'
+    # Endpoint to test: https://myxxxxx.crm.ondemand.com/sap/c4c/odata/v1/c4codataapi/EmployeeUserSubscriptionAssignmentUserSubscriptionTypeCodeCollection?$format=json&$filter=Code eq '8003186'
 
     @staticmethod
     def name():
         return "tap_tester_sap_sales_service_cloud_automatic_fields"
 
+    def streams_having_duplicated_records(self):
+        """
+            Returns the set of streams having few identical records with same primary key values.
+            This causes our fix to have all keys as composite keys to fail as tap-tester
+            expects unique values for primary key fields.
+        """
+
+        return {
+            "contract_item_party_state_code_collection",
+            "lead_individual_customer_address_state_code_collection",
+            "contract_item_party_po_box_deviating_state_code_collection"
+        }
+
     def streams_to_test(self):
-        return self.expected_stream_names() - self.streams_to_exclude()
+        return self.expected_stream_names() - self.streams_to_exclude() - self.streams_having_duplicated_records()
